cbda4a9d3698b1138ae8fdae8b9097b2d853bb79
Sebastian <s.mazur.praca@gmail.com>
2018-07-21 11:57:29 +0200

    Fix server file

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
+var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, function () {
+    console.log('Our app is running on http://localhost:' + port);
+});
